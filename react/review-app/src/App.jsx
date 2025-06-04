e86c69cfee82a981a1e0fa0eb298bb0e1f333a99
Jagan <110019353+jaganjavid@users.noreply.github.com>
2025-06-04 19:42:53 +0530

    Added a list items

--- react/review-app/src/App.jsx
+++ react/review-app/src/App.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackForm from "./components/FeedbackForm";
 
 const App = () => {
+
+
+  const [feedback, setFeedback] = useState([
+    {
+        id:1,
+        text:"This is a sample 1"
+    },
+    {
+        id:2,
+        text:"This is a sample 2"
+    },
+    {
+        id:3,
+        text:"This is a sample 3"
+    },
+  ]);
+
+
+  const deleteFeedback = (id) => {
+   
+    if(window.confirm("Are your sure?")){
+      setFeedback(feedback.filter(item => item.id !== id))
+    }
+
+  }
+
+  
   return (
    <>
      <Header/>
-     <Header text='Hello' bgColor='green' textColor='black'/>
+
+     <div className='container'>
+       <FeedbackForm/>
+       <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+     </div>
    </>
   )
 }
---
+++ react/review-app/src/components/FeedbackItem.jsx
@@ -0,0 +1,28 @@
+import { MdEdit } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+
+const FeedbackItem = ({item, handleDelete}) => {
+
+    
+  return (
+    <div className='card'>
+        
+        <div className='card-wrapper'>
+          <h4>{item.text}</h4>
+
+          <div>
+            <div className="edit">
+              <MdEdit size="20px"/>
+            </div>
+
+            <div className="delete">
+              <MdDelete size="20px" onClick={() => handleDelete(item.id)}/>
+            </div>
+          </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default FeedbackItem
